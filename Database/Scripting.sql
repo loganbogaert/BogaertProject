ac4edb03d539c9f0d93949be50397f50e16f48e8
loganbogaert <32387016+loganbogaert@users.noreply.github.com>
2019-03-30 19:01:42 +0100

    Add files via upload

---
+++ Database/Scripting.sql
@@ -0,0 +1,36 @@
+---------------<use master to drop TombolaDB>---------------
+use master
+---------------<drop database if not exits>-----------------
+if DB_ID('TombolaDB') is not null drop database TombolaDB
+---------------<create database if  exits>------------------
+if DB_ID('TombolaDB') is null create database TombolaDB
+---------------<start using principal database>-------------
+use TombolaDB
+---------------<Create user table>--------------------------
+create table Users 
+(
+Id int not null primary key identity,
+Name varchar(50) not null, 
+Email varchar(80) not null, 
+Password varchar(50) not null
+)
+
+---------------<Create items table>--------------------------
+create table Items
+(
+Id int not null primary key identity, 
+Name varchar(50) not null, 
+Description text not null,
+Raffler int foreign key references Users(Id) not null,
+RafflePrice money not null,
+CurrentAmount money null
+)
+---------------<Create transaction table>--------------------------
+create table TransactionTable
+(
+Id int not null primary key identity, 
+ItemId int not null foreign key references Items(Id),
+Donor int not null foreign key references Users(Id),
+Amount money not null
+)
+
